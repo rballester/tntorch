b8fae352501f4e943682f3a2e1351724038c4a3a
Mikhail Usvyatsov <m.usvyatsov@gmail.com>
2019-08-28 01:16:10 +0200

    fixed problems with differentiation

--- tntorch/round.py
+++ tntorch/round.py
@@ -107,7 +107,7 @@ def truncated_svd(M, delta=None, eps=None, rmax=None, left_ortho=True, algorithm
         w, v = torch.symeig(gram, eigenvectors=True)
         if verbose:
             print('Time (symmetric EIG):', time.time() - start)
-        w[w < 0] = 0
+        w = torch.where(w < 0, torch.zeros_like(w), w)
         w = torch.sqrt(w)
         svd = [v, w]
         # Sort eigenvalues and eigenvectors in decreasing importance
