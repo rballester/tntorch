d255773632a5da6de4092798d9e58854abda3082
rballester <rballri@gmail.com>
2021-10-08 09:44:43 +0200

    Added matrix trace()

--- tests/test_tt_matrix.py
+++ tests/test_tt_matrix.py
@@ -1,10 +1,23 @@
 import tntorch as tn
 import pytest
+import numpy as np
 import torch
 torch.set_default_dtype(torch.float64)
 
 
 torch.manual_seed(1)
+
+
+def test_construction():
+    m = torch.rand(11 * 3, 23 * 2)
+
+    input_dims = [11, 3]
+    output_dims = [23, 2]
+    ranks = [50]
+
+    ttm = tn.TTMatrix(m, input_dims=input_dims, output_dims=output_dims, ranks=ranks)
+    assert torch.allclose(m, ttm.torch())
+
 
 def test_tt_multiply():
     m = torch.rand(11 * 3, 23 * 2)
@@ -18,12 +31,11 @@ def test_tt_multiply():
     assert torch.allclose(v @ m, tn.tt_multiply(ttm, v))
 
 
-def test_construction():
-    m = torch.rand(11 * 3, 23 * 2)
-
+def test_trace():
     input_dims = [11, 3]
-    output_dims = [23, 2]
+    output_dims = input_dims
+    m = torch.rand(np.prod(input_dims), np.prod(output_dims))
     ranks = [50]
 
     ttm = tn.TTMatrix(m, input_dims=input_dims, output_dims=output_dims, ranks=ranks)
-    assert torch.allclose(m, ttm.full())
+    assert torch.allclose(torch.trace(m), ttm.trace())
--- tntorch/tt_matrix.py
+++ tntorch/tt_matrix.py
@@ -4,35 +4,16 @@ import itertools
 from typing import Any, Sequence
 
 
-def tt_multiply(tt_matrix: Any, tensor: torch.Tensor):
-    ''' Multiply TTMatrix by any tensor of more than 1-way.
-    
-    For vectors, reshape them to matrix of shape 1 x I
-    '''
-    assert len(tensor.shape) > 1
-    result = tensor.reshape([-1] + [tt_matrix.input_dims[-1]] + [1])
-    shape = torch.tensor(tensor.shape)
-    b = torch.prod(shape[:-1])
-
-    for d in reversed(range(tt_matrix.d)):
-        core = tt_matrix.cores[d]
-        result = torch.einsum('aijb,rib->jra', core, result)
-        if d > 0:
-            result = result.reshape(-1, tt_matrix.input_dims[d - 1], core.shape[0])
-
-    output_dim = torch.prod(tt_matrix.output_dims)
-    result = result.reshape(output_dim, b)
-    return result.T.reshape(list(shape[:-1]) + [output_dim])
-
-
 class TTMatrix:
-    '''TTMatrix efficiently stores matrix in TT-like format.
+    """
+    TTMatrix efficiently stores matrix in TT-like format.
 
     Input matrix M is of shape IxO is reshaped into d-way tensor t of shape
     i_0 x o_o, ... i_{d - 1} x o_{d - 1}.
     TT representation of t is computed and each of d TT cores is reshaped
     from r_j, t.shape[j], r_{j + 1} to r_j, i_j, o_j, r_{j + 1}.
-    '''
+    """
+
     def __init__(
             self,
             M: torch.Tensor,
@@ -68,7 +49,13 @@ class TTMatrix:
             core.reshape(core.shape[0], input_dims[i], output_dims[i], core.shape[-1])
             for i, core in enumerate(tt.cores)]
 
-    def full(self):
+    def torch(self):
+        """
+        Decompress into a PyTorch 2D tensor
+
+        :return: a 2D torch.tensor
+        """
+
         cores = [core.reshape(core.shape[0], -1, core.shape[-1]) for core in self.cores]
         tensor = tn.Tensor(cores).torch()
         input_dims = self.input_dims
@@ -83,3 +70,51 @@ class TTMatrix:
         dims = list(range(2 * d))
         tensor = tensor.permute(dims[0::2] + dims[1::2])
         return tensor.reshape(input_size, output_size)
+
+    def numpy(self):
+        return self.torch().numpy()
+
+    def trace(self):
+        """
+        Compute the trace of a TTMatrix.
+
+        :return: a scalar
+        """
+
+        factor = torch.ones(1)
+        for c in self.cores:
+            factor = torch.einsum('i,iaaj->j', factor, c)
+        return factor[0]
+
+    def flatten(self):
+        """
+        Flattens this TTMatrix into a compressed vector. For each core, its input and output dimension will be grouped together into a single spatial dimension.
+
+        :return: a `tn.Tensor`
+        """
+
+        return tn.Tensor([c.reshape(c.shape[0], -1, c.shape[-1]) for c in self.cores])
+
+
+def tt_multiply(tt_matrix: TTMatrix, tensor: torch.Tensor):
+    """
+    Multiply TTMatrix by any tensor of more than 1-way.
+
+    For vectors, reshape them to matrix of shape 1 x I
+    """
+
+    assert len(tensor.shape) > 1
+
+    result = tensor.reshape([-1] + [tt_matrix.input_dims[-1]] + [1])
+    shape = torch.tensor(tensor.shape)
+    b = torch.prod(shape[:-1])
+
+    for d in reversed(range(tt_matrix.d)):
+        core = tt_matrix.cores[d]
+        result = torch.einsum('aijb,rib->jra', core, result)
+        if d > 0:
+            result = result.reshape(-1, tt_matrix.input_dims[d - 1], core.shape[0])
+
+    output_dim = torch.prod(tt_matrix.output_dims)
+    result = result.reshape(output_dim, b)
+    return result.T.reshape(list(shape[:-1]) + [output_dim])
